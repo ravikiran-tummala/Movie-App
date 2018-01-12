6d3bb491a5b92ffef26cf720109fc43835a60b1e
Ravi Kiran Tummala <ravikirantummala@ravis-mbp.asia.kofax.com>
2018-01-12 14:46:51 +0530

    Added Constants and Network Library

---
+++ MovieApp/app/src/main/java/com/ravikirantummala/movieapp/Common/AppConstants.java
@@ -0,0 +1,9 @@
+package com.ravikirantummala.movieapp.Common;
+
+/**
+ * Created by ravikirantummala on 10/01/18.
+ */
+
+public class AppConstants {
+    public static final String API_KEY = "Paste your api key here";
+}
---
+++ MovieApp/app/src/main/java/com/ravikirantummala/movieapp/Services/ServiceFactory.java
@@ -0,0 +1,9 @@
+package com.ravikirantummala.movieapp.Services;
+
+/**
+ * Created by ravikirantummala on 10/01/18.
+ */
+
+public class ServiceFactory {
+
+}
