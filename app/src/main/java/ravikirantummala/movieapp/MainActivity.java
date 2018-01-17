a53474b8cbfc5a56d28d26da14d83626b3d1f676
Ravi Kiran Tummala <ravikirantummala@ravis-mbp.asia.kofax.com>
2018-01-17 11:55:51 +0530

    Added code for the first service

--- app/src/main/java/ravikirantummala/movieapp/MainActivity.java
+++ app/src/main/java/ravikirantummala/movieapp/MainActivity.java
@@ -1,22 +1,39 @@
 package ravikirantummala.movieapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ravikirantummala.movieapp.Models.MovieListModel;
 import ravikirantummala.movieapp.Services.ServerListener;
-import ravikirantummala.movieapp.Services.ServiceFactory;
+
+import static ravikirantummala.movieapp.Services.ServiceFactory.getPopularMovieList;
 
 public class MainActivity extends AppCompatActivity implements ServerListener {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ServiceFactory.getPopularMovieList(1,this,this);
+        getPopularMovieList(1,this,this);
+
     }
 
     @Override
     public void onSuccessResponse(String response) {
+        JSONObject jsonResponse = null;
+        try {
+            jsonResponse = new JSONObject(response);
+            MovieListModel movieListModel = new MovieListModel(jsonResponse);
+            Log.e(LOG_TAG,movieListModel.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- app/src/main/java/ravikirantummala/movieapp/Services/ServiceFactory.java
+++ app/src/main/java/ravikirantummala/movieapp/Services/ServiceFactory.java
@@ -8,6 +8,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+
 import ravikirantummala.movieapp.Common.AppConstants;
 
 /**
@@ -19,15 +20,18 @@ import ravikirantummala.movieapp.Common.AppConstants;
 public class ServiceFactory {
 
     private final static String baseURL = "https://api.themoviedb.org/3/";
-    private final static String popularMoviePath = "movie/popular";
+    private final static String movieTag = "movie";
+    private final static String popularTag = "popular";
     private final static String language = "en-US";
+    private final static String LOG_TAG = ServiceFactory.class.getSimpleName();
 
     public static void getPopularMovieList(int page, Context context, final ServerListener listener){
 
         Uri.Builder builder = Uri.parse(baseURL).buildUpon().
-                appendPath(popularMoviePath).
-                appendQueryParameter(AppConstants.LANGUAGE_KEY,language).
+                appendPath(movieTag).
+                appendPath(popularTag).
                 appendQueryParameter(AppConstants.API_KEY,AppConstants.SERVICE_API_KEY).
+                appendQueryParameter(AppConstants.LANGUAGE_KEY,language).
                 appendQueryParameter(AppConstants.PAGE_KEY,String.valueOf(page));
 
 
@@ -38,12 +42,13 @@ public class ServiceFactory {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        Log.d(LOG_TAG,response);
                         listener.onSuccessResponse(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("Server Error",error.getMessage());
+                Log.e(LOG_TAG,error.getMessage());
                 listener.onErrorResponse(error);
             }
         });
