f6ca994bddd537fafaa2003952fc32ecbbca4716
Ravi Kiran Tummala <ravikirantummala@Ravis-MacBook-Pro.local>
2018-01-17 07:21:29 +0530

    Initial commit after recreating the project

---
+++ app/src/main/java/ravikirantummala/movieapp/Common/AppConstants.java
@@ -0,0 +1,33 @@
+package ravikirantummala.movieapp.Common;
+
+/**
+ * Created by ravikirantummala on 10/01/18.
+ */
+
+public class AppConstants {
+    public static final String SERVICE_API_KEY = "Paste your api key here";
+
+    // Request Keys
+    public static final String PAGE_KEY = "page";
+    public static final String API_KEY = "api_key";
+    public static final String LANGUAGE_KEY = "language";
+    // JSON Response Keys
+    public static final String POSTER_PATH = "poster_path";
+    public static final String ADULT = "adult";
+    public static final String OVERVIEW = "overview";
+    public static final String RELEASE_DATE = "release_date";
+    public static final String GENRE_IDS = "genre_ids";
+    public static final String ID = "id";
+    public static final String ORIGINAL_TITLE = "original_title";
+    public static final String ORIGINAL_LANGUAGE = "original_language";
+    public static final String TITLE = "title";
+    public static final String BACKDROP_PATH = "backdrop_path";
+    public static final String POPULARITY = "popularity";
+    public static final String VOTE_COUNT = "vote_count";
+    public static final String VIDEO = "video";
+    public static final String VOTE_AVERAGE = "vote_average";
+    public static final String PAGE = "page";
+    public static final String RESULTS = "results";
+    public static final String TOTAL_RESULTS = "total_results";
+    public static final String TOTAL_PAGES = "total_pages";
+}
---
+++ app/src/main/java/ravikirantummala/movieapp/Services/ServiceFactory.java
@@ -0,0 +1,52 @@
+package ravikirantummala.movieapp.Services;
+
+import android.content.Context;
+import android.net.Uri;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import ravikirantummala.movieapp.Common.AppConstants;
+
+/**
+ * Created by ravikirantummala on 10/01/18.
+ */
+
+
+
+public class ServiceFactory {
+
+    private final static String baseURL = "https://api.themoviedb.org/3/";
+    private final static String popularMoviePath = "movie/popular";
+    private final static String language = "en-US";
+
+    public static void getPopularMovieList(int page, Context context, final ServerListener listener){
+
+        Uri.Builder builder = Uri.parse(baseURL).buildUpon().
+                appendPath(popularMoviePath).
+                appendQueryParameter(AppConstants.LANGUAGE_KEY,language).
+                appendQueryParameter(AppConstants.API_KEY,AppConstants.SERVICE_API_KEY).
+                appendQueryParameter(AppConstants.PAGE_KEY,String.valueOf(page));
+
+
+        String url = builder.toString();
+        Singleton singleton = Singleton.getInstance(context);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        listener.onSuccessResponse(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.onErrorResponse(error);
+            }
+        });
+
+        singleton.addToRequestQueue(stringRequest);
+    }
+
+}
